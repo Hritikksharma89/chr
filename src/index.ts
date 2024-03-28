7aee47aa9f4ce9ebe4689c5b16e5bc9f5ee3848c
Jayant Rohila <jayant@bnimile.com>
2024-03-28 21:52:29 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import express, { Application } from 'express'
+import { env } from './environment/environment'
+import { CONSTANT, ROUTE } from './constant'
+import { logger } from './log/log'
+import { errorHandler } from './middleware/error.middleware'
+import mongoose from 'mongoose'
+import userRoute from './controller/user.controller'
+
+const app: Application = express()
+const PORT = env.PORT
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(express.json())
+
+// Connect to MongoDB
+mongoose
+  .connect(env.DATABASE_URL)
+  .then(() => {
+    console.log('Connected to MongoDB')
+  })
+  .catch((error) => console.log(error.message))
+
+// Define routes
+app.use('/users', userRoute)
+
+// Error handling middleware
+app.use(errorHandler)
+
+app.listen(PORT, () => {
+  try {
+    logger.info(CONSTANT.SERVER_RUNNING + PORT, CONSTANT.SUCCESS)
+  } catch (error) {
+    logger.error(CONSTANT.SERVER_ERROR, error)
+  }
+})
+
+process.on(CONSTANT.UNHANDLED_REJECTION, (reason, promise) => {
+  logger.error(CONSTANT.UNHANDLED_REJECTION, promise, CONSTANT.REASON, reason)
+  process.exit(1)
+})
